63d234fca211f647d69b34ebdf97f468d9658389
Immobile07 <arnobfarhan070@gmail.com>
2025-05-29 20:39:08 +0600

    first commit

---
+++ double_naki_trple.js
@@ -0,0 +1,25 @@
+console.log(50=="50");
+console.log(50==="50");
+//Both of them are strings that's why
+console.log("JavaScript"=='JavaScript');
+console.log("JavaScript"==='JavaScript');
+//this checks the datatype first
+console.log(25==="25");
+//this checks the value no data type match is checked
+console.log(25=="25");
+
+console.log("apple"=="apple");
+console.log("apple"==="apple");
+
+console.log("test"=="Test");
+console.log("test"==="Test");
+
+const variableX=15;
+const variableY="20";
+console.log(variableX!=variableY);
+console.log(variableX!==variableY);
+
+const variableA="hello";
+const variableB="Hello";
+console.log(variableA==variableB);
+console.log(variableA===variableB);
---
+++ kanamachi_true_kanamachi_false.js
@@ -0,0 +1,16 @@
+const isbamboo=true;
+console.log(isbamboo);
+const isreply=false;
+console.log(isreply);
+const celebration=true;
+console.log(celebration)
+const isHappy=true;
+const hasMoney=false;
+const isHungry=true;
+console.log(isHappy,hasMoney,isHungry)
+const isStudyDone=false;
+console.log(isStudyDone);
+const isPhoneCharged=true;
+console.log(isPhoneCharged)
+const isDinnerDone=false;
+console.log(isDinnerDone)
